4bddb89bffdfb2b697dda1f37c421d33659e7591
Didier Raboud <odyx@debian.org>
2014-07-28 08:44:07 +0200

    1.0.55-1 (patches unapplied)
    Imported using git-ubuntu import.

--- filter/imagetoraster.c
+++ filter/imagetoraster.c
@@ -36,6 +36,7 @@
  */
 
 #include "common.h"
+#include <cupsfilters/colord.h>
 #include <cupsfilters/image-private.h>
 #include <unistd.h>
 #include <math.h>
@@ -191,8 +192,9 @@ main(int  argc,				/* I - Number of command-line arguments */
   int			plane,		/* Current color plane */
 			num_planes;	/* Number of color planes */
   char			filename[1024];	/* Name of file to print */
-  int                   cm_off;         /* Is color management off? */
-
+  int                   cm_calibrate;   /* Are we color calibrating the device? */
+  int                   device_inhibited;
+  char                  tmpstr[1024];
 
  /*
   * Make sure status messages are not buffered...
@@ -216,6 +218,8 @@ main(int  argc,				/* I - Number of command-line arguments */
 	    argv[0]);
     return (1);
   }
+
+  cm_calibrate = 0;
 
  /*
   * See if we need to use the imagetops and pstoraster filters instead...
@@ -493,11 +497,17 @@ main(int  argc,				/* I - Number of command-line arguments */
   else
     resolution = "";
 
-  /* support the "no-color-management" option */
-  if ((cupsGetOption("no-color-management", num_options, options)) == NULL)
-    cm_off = 1;
-  else
-    cm_off = 0;
+  snprintf (tmpstr, sizeof(tmpstr), "cups-%s", getenv("PRINTER"));
+  device_inhibited = colord_get_inhibit_for_device_id (tmpstr);
+
+  /* support the "cm-calibration" option */
+  if ((cupsGetOption("cm-calibration", num_options, options)) != NULL) {
+    cm_calibrate = 1;
+    device_inhibited = 1;
+  }
+
+  fprintf(stderr, "DEBUG: Color Management: %s\n", cm_calibrate ?
+          "Calibration Mode/Enabled" : "Calibration Mode/Off");
 
  /*
   * Choose the appropriate colorspace...
@@ -612,7 +622,7 @@ main(int  argc,				/* I - Number of command-line arguments */
   */
    
   if ((val = cupsGetOption("profile", num_options, options)) != NULL &&
-      !cm_off)
+      !device_inhibited)
   {
     profile = &userprofile;
     sscanf(val, "%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f",
@@ -636,8 +646,7 @@ main(int  argc,				/* I - Number of command-line arguments */
     userprofile.matrix[2][1] *= 0.001f;
     userprofile.matrix[2][2] *= 0.001f;
   }
-  else if (ppd != NULL &&
-           !cm_off)
+  else if (ppd != NULL && !device_inhibited)
   {
     fprintf(stderr, "DEBUG: Searching for profile \"%s/%s\"...\n",
             resolution, media_type);
