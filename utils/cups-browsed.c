a5725fa6c96c932069d635e8d1255234f621fb45
Ubuntu Git Importer <usd-importer-do-not-mail@canonical.com>
2020-06-09 22:32:42 +0000

    New upstream version 1.25.8

--- utils/cups-browsed.c
+++ utils/cups-browsed.c
@@ -5803,11 +5803,8 @@ get_printer_attributes(const char* uri, int fallback_request,
 
         if(!strcmp(ippGetName(attr),"status-message") && 
           !strcmp(valuebuffer,"server-error-version-not-supported")){
-	  if (!fallback_request) {
-	    debug_printf("The server doesn't support IPP2.0 request, trying to IPP1.1 request\n");
-	    httpClose(http_printer);
-	    return get_printer_attributes(uri,1,pattrs,job_state_attributes, attr_size);
-	  }
+          debug_printf("The server doesn't support IPP2.0 request, trying to IPP1.1 request\n");
+          return get_printer_attributes(uri,1,pattrs,job_state_attributes, attr_size);
         }
        attr = ippNextAttribute(response);
       }
@@ -5815,13 +5812,9 @@ get_printer_attributes(const char* uri, int fallback_request,
   } else{
     debug_printf("Request for IPP attributes (get-printer-attributes) for printer with URI %s failed: %s\n",
      uri, cupsLastErrorString());
-    if (!fallback_request) {
-      debug_printf("Trying IPP1.1 Request\n");
-      httpClose(http_printer);
-      return get_printer_attributes(uri,1,pattrs,job_state_attributes, attr_size);
-    }
+    debug_printf("Trying IPP1.1 Request\n");
+    return get_printer_attributes(uri,1,pattrs,job_state_attributes, attr_size);
   }
-  httpClose(http_printer);
 
   return response;
 }
